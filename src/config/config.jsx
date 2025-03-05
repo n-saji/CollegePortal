5c6a7e2c6a521896989f89557812e2f2419abddd
Nikhil Saji <nikhilsaji200@gmail.com>
2025-03-05 17:03:53 -0500

    Updated config file

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,5 +1,5 @@
 export const API_URL = "https://dolphin-app-2zya2.ondigitalocean.app";
-export const SOCKET_URL = "ws://dolphin-app-2zya2.ondigitalocean.app:8080/ws";
+export const SOCKET_URL = "wss://dolphin-app-2zya2.ondigitalocean.app:8080/ws";
 
 // export const API_URL = "http://localhost:5050";
 // export const SOCKET_URL = "ws://localhost:8080/ws";
