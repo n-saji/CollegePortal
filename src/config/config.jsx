1b9fa40f2881d7d544a20db8835b4e4ff0557d3e
Nikhil Saji <nikhilsaji200@gmail.com>
2025-03-05 17:03:17 -0500

    Added web sockets code

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,3 +1,10 @@
-export const API_URL = "https://dolphin-app-2zya2.ondigitalocean.app";
+// export const API_URL = "https://dolphin-app-2zya2.ondigitalocean.app";
+// export const SOCKET_URL = "ws://dolphin-app-2zya2.ondigitalocean.app:8080/ws";
+
 // export const API_URL = "http://localhost:5050";
+// export const SOCKET_URL = "ws://localhost:8080/ws";
+
+export const API_URL = "http://3.15.188.218:5050";
+export const SOCKET_URL = "ws://3.15.188.218:8080/ws";
+
 export const BASE_URL = "";
