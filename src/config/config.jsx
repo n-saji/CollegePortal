f1b673ae900f0b9a3a0461010cec0ae224b09a62
Nikhil Saji <nikhilsaji200@gmail.com>
2025-03-06 00:32:45 -0500

    Web socket bug fixed

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,10 +1,12 @@
+const protocol = window.location.protocol.includes('https') ? 'wss' : 'ws'
+
 export const API_URL = "https://dolphin-app-2zya2.ondigitalocean.app";
-export const SOCKET_URL = "wss://dolphin-app-2zya2.ondigitalocean.app:8080/ws";
+export const SOCKET_URL = protocol + "://dolphin-app-2zya2.ondigitalocean.app/ws";
 
 // export const API_URL = "http://localhost:5050";
-// export const SOCKET_URL = "ws://localhost:8080/ws";
+// export const SOCKET_URL = protocol + '://localhost:5050/ws';
 
 // export const API_URL = "http://3.15.188.218:5050";
-// export const SOCKET_URL = "ws://3.15.188.218:8080/ws";
+// export const SOCKET_URL = protocol + "://3.15.188.218:8080/ws";
 
 export const BASE_URL = "";
