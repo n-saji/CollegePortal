431dfdd6376c3e08b0fa2e160d4422effc34407a
Nikhil Saji <nikhilsaji200@gmail.com>
2025-03-15 00:54:01 -0400

    WEB SOCKETS Updates 2

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,10 +1,10 @@
-const protocol = window.location.protocol.includes('https') ? 'wss' : 'ws'
+const protocol = window.location.protocol.includes('https') ? 'wss' : 'wss'
 
-// export const API_URL = "https://dolphin-app-2zya2.ondigitalocean.app";
-// export const SOCKET_URL = protocol + "://dolphin-app-2zya2.ondigitalocean.app/ws";
+export const API_URL = "https://dolphin-app-2zya2.ondigitalocean.app";
+export const SOCKET_URL = protocol + "://dolphin-app-2zya2.ondigitalocean.app/ws";
 
-export const API_URL = "http://localhost:5050";
-export const SOCKET_URL = protocol + '://localhost:5050/ws';
+// export const API_URL = "http://localhost:5050";
+// export const SOCKET_URL = protocol + '://localhost:5050/ws';
 
 // export const API_URL = "http://3.15.188.218:5050";
 // export const SOCKET_URL = protocol + "://3.15.188.218:8080/ws";
