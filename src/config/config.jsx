682d7d0d47d4f94152e9cdf97bab5f22b7b5daed
Nikhil Saji <nikhilsaji200@gmail.com>
2025-04-09 11:34:19 -0400

    Updated config urls

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,12 +1,19 @@
 const protocol = window.location.protocol.includes('https') ? 'wss' : 'ws'
 
-export const API_URL = "https://dolphin-app-2zya2.ondigitalocean.app";
-export const SOCKET_URL = protocol + "://dolphin-app-2zya2.ondigitalocean.app/ws";
+// For production
+export const API_URL = "https://urchin-app-r3229.ondigitalocean.app";
+export const SOCKET_URL = protocol + "://urchin-app-r3229.ondigitalocean.app/ws";
 
+// For local development
 // export const API_URL = "http://localhost:5050";
 // export const SOCKET_URL = protocol + '://localhost:5050/ws';
 
+export const BASE_URL = "";
+
+
+// Deprecated URLs
+// export const API_URL = "https://dolphin-app-2zya2.ondigitalocean.app";
+// export const SOCKET_URL = protocol + "://dolphin-app-2zya2.ondigitalocean.app/ws";
+
 // export const API_URL = "http://3.15.188.218:5050";
 // export const SOCKET_URL = protocol + "://3.15.188.218:8080/ws";
-
-export const BASE_URL = "";
